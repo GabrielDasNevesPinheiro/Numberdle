a2f6099039e029f6ed7a7ecc5cceaff6dffcf4d3
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-23 14:26:10 -0300

    Slash commands definition

---
+++ src/api/Commands.js
@@ -0,0 +1,25 @@
+
+const commands = [
+    {
+        name: "setChannel",
+        description: "Set Numberdle's default channel."
+    },
+    {
+        name: "gameRules",
+        description: "Shows Numberdle game rules."
+    },
+    {
+        name: "nextNumberdle",
+        description: "When next random number comes?"
+    },
+    {
+        name: "rank",
+        description: "Shows ranking."
+    },
+    {
+        name: "localRank",
+        description: "Shows server's rank."
+    }
+];
+
+export default commands;
