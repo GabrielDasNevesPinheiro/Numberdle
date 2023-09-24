6b924d79723295d195f09edc5c1685b82ec77ea3
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-24 15:14:05 -0300

    New Slash commands impl

--- src/api/Register.js
+++ src/api/Register.js
@@ -1,6 +1,10 @@
 import { REST, Routes } from 'discord.js';
 import { config } from 'dotenv';
-import commands from './Commands.js';
+import setChannel from './commands/SetChannel.js';
+import gameRules from './commands/GameRules.js';
+import localRank from './commands/LocalRank.js'
+import rank from './commands/Rank.js';
+import nextNumber from './commands/NextNumber.js';
 
 config();
 
@@ -8,6 +12,14 @@ const token = process.env.TOKEN;
 const client_id = process.env.CLIENT_ID;
 
 const rest = new REST({ version: '10' }).setToken(token);
+
+const commands = [
+    setChannel.toJSON(),
+    gameRules.toJSON(),
+    localRank.toJSON(),
+    rank.toJSON(),
+    nextNumber.toJSON()];
+
 
 try {
     
