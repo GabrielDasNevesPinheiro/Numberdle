38519cd48f6cc555b660b3f39ff9875447742894
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-25 14:49:24 -0300

    Guild onJoin message

--- src/core/app.ts
+++ src/core/app.ts
@@ -10,11 +10,17 @@ client.on('ready', () => {
     console.log(`Running... ${client.user?.tag}`);
 });
 
+client.on('guildCreate', (guild) => {
+
+    client.users.send(guild.ownerId, "Obrigado por me adicionar em seu servidor, para me configurar basta definir um canal padrão para mim utilizando /setchannel em seu servidor! :)");
+
+});
+
 client.on('interactionCreate', async (interaction) => {
 
     if (!interaction.isChatInputCommand()) return;
 
-    executeAction(interaction.commandName, interaction)
-})
+    executeAction(interaction.commandName, interaction);
+});
 
 client.login(process.env.TOKEN);
