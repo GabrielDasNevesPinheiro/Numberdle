25257ab8915096c10eb2dc398567518d1cce67de
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-25 17:33:04 -0300

    SetChannel db test logic

--- src/database/Testing.ts
+++ src/database/Testing.ts
@@ -1,9 +1,18 @@
 import sequelize from "./Connection";
+import Guild from "./Models/Guild";
 
 
 async function test() {
         
     await sequelize.authenticate();
+    await sequelize.sync();
+
+    const guilds = await Guild.findAll();
+
+    guilds.map((guild) => {
+        console.log(guild.defaultChannel, guild.guildId);
+    });
+    
     console.log("Connected.");
 }
 
