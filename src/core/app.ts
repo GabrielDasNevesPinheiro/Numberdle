c143d843524bf448d77a8781e8a6deaa690cbd6b
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-25 17:32:49 -0300

    Guild on Join database data init

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,17 +1,26 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
 import executeAction from "../handlers/InteractionHandler";
+import sequelize from "../database/Connection";
+import Guild from "../database/Models/Guild";
 
 config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`Running... ${client.user?.tag}`);
+    await sequelize.authenticate();
+    await sequelize.sync();
 });
 
-client.on('guildCreate', (guild) => {
+client.on('guildCreate', async (guild) => {
 
+    await Guild.create({
+        guildId: guild.id,
+        defaultChannel: null
+    });
+    
     client.users.send(guild.ownerId, "Obrigado por me adicionar em seu servidor, para me configurar basta definir um canal padrão para mim utilizando /setchannel em seu servidor! :)");
 
 });
