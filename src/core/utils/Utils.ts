55785f6e4cda7f116ec1780e804c7a06293cc7ee
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-26 13:36:44 -0300

    utility functions file

---
+++ src/core/utils/Utils.ts
@@ -0,0 +1,13 @@
+// this file contains utility functions
+
+function getTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    let month = today.getMonth();
+    let day = today.getDate();
+    
+    month = month < 10 ? Number(`0${month}`) : month;
+    day = day < 10 ? Number(`0${day}`) : day;
+
+    return new Date(year, month, day);
+}
