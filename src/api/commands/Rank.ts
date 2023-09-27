1a7de47cde6e325457a8dcbfa87067ad4a2bdb8c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-27 12:39:35 -0300

    Ranking initial logic

--- src/api/commands/Rank.ts
+++ src/api/commands/Rank.ts
@@ -1,5 +1,6 @@
-import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CacheType, Colors, CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import Command from "./Command";
+import Player from "../../database/Models/Player";
 
 export default abstract class Rank extends Command {
 
@@ -7,8 +8,26 @@ export default abstract class Rank extends Command {
         .setName("rank")
         .setDescription("Shows global rank.");
     
-    static execute(interaction: CommandInteraction<CacheType>): void {
-        interaction.reply("Here's the rank.");
+    static async execute(interaction: CommandInteraction<CacheType>) {
+
+        const embed = new EmbedBuilder().setTitle(":trophy: Melhores jogadores :trophy:").setColor(Colors.Red);
+
+        const players = (await Player.findAll({ order: [['score', 'DESC']]})).slice(0, 10);
+
+        players.map((player) => {
+
+            const index = players.indexOf(player) + 1;
+            let emoji = `${index}`;
+            if (index == 1) emoji = ':first_place:' 
+            if (index == 2) emoji = ':second_place:' 
+            if (index == 3) emoji = ':third_place:' 
+
+            embed.addFields([
+                { name: `${emoji} - ${player.username}`, value: `${player.score} Pontos` }
+            ])
+        })
+
+        await interaction.reply({ embeds: [embed], content: "Veja a seguir o ranking", ephemeral: true });
     }
 
 }
