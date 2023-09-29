adb51761848b5c5ac6dbc775dfab8096beb474a9
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-29 01:53:49 -0300

    Rework in Game Rules reply

--- src/api/commands/GameRules.ts
+++ src/api/commands/GameRules.ts
@@ -1,4 +1,4 @@
-import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CacheType, Colors, CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import Command from "./Command";
 
 export default abstract class GameRules extends Command {
@@ -8,9 +8,30 @@ export default abstract class GameRules extends Command {
         .setDescription("Shows Numberdle game rules.");
 
     static async execute(interaction: CommandInteraction<CacheType>) {
+
+        const embed = new EmbedBuilder().setTitle("Numberdle")
+            .setThumbnail('https://images.squarespace-cdn.com/content/v1/50eca855e4b0939ae8bb12d9/1371867381406-C3XPXES7PXTPNQPOXFHD/Black_Number.007.jpg')
+            .addFields({
+                name: "Como jogar? :thinking:",
+                value: "Você pode jogar usando meu comando /play"
+            },
+            {
+                name: "Quando você der play... :point_up:",
+                value: "Irei gerar um número aleatório entre 0 e 1000 e você terá que adivinhar."
+            },
+            {
+                name: "Quantas tentativas? :warning:",
+                value: "Você tem só 10 tentativas e só joga uma vez por dia."
+            }, 
+            {
+                name: "Ganhe pontos! :money_with_wings:",
+                value: "Você ganhará muitos pontos se acertar com poucas tentativas."
+            }).setColor(Colors.Red);
+
         await interaction.reply({ 
             ephemeral: true,
-            content: "Diariamente eu irei gerar um número aleatório e cabe a você advinhar que número é esse, você terá apenas 10 chances. Você ganhará mais pontos se acertar com poucas tentativas... Boa sorte! :nerd:"
+            content: "Eis as regras :nerd: :point_up:",
+            embeds: [embed]
         });
     }
 
