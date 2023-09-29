962230499125f4de4d06d6f282ce9e26d56d2f8c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-09-28 21:53:12 -0300

    Local and Global rank + emoji conditional remove

--- src/api/commands/Rank.ts
+++ src/api/commands/Rank.ts
@@ -1,31 +1,54 @@
 import { CacheType, Colors, CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import Command from "./Command";
 import Player from "../../database/Models/Player";
+import Guild from "../../database/Models/Guild";
 
 export default abstract class Rank extends Command {
 
     static command: SlashCommandBuilder = new SlashCommandBuilder()
+        .addStringOption((option) =>
+            option.setName("target")
+                .setDescription("Tipo de ranking a ser consultado")
+                .addChoices(
+                    { name: "Ranking Global", value: "global" },
+                    { name: "Ranking Local", value: "local" }
+                ).setRequired(true)
+        )
         .setName("rank")
         .setDescription("Shows global rank.");
-    
+
     static async execute(interaction: CommandInteraction<CacheType>) {
 
         const embed = new EmbedBuilder().setTitle(":trophy: Melhores jogadores :trophy:").setColor(Colors.Red);
 
-        const players = (await Player.findAll({ order: [['score', 'DESC']]})).slice(0, 10);
+        let players: Player[] = [];
+        
+        const { value: target } = interaction.options.get("target");
+        
+        if (target === "global"){
+            players = (await Player.findAll({ order: [['score', 'DESC']] })).slice(0, 10);
+        }
+        
+        if (target === "local") {
+            const guildPlayers = (await Guild.findOne({ where: { guildId: interaction.guildId } })).players;
+            await Promise.all(guildPlayers.map(async (userId) => {
+                
+                const guildPlayer = await Player.findOne({ where: { userId } });
+                players.push(guildPlayer);
+                players.sort((first, next) => first.score > next.score ? -1 : 1);
+            }));
+        }
 
+        let emojis = [':first_place:', ':second_place:', ':third_place:'];
         players.map((player) => {
 
-            const index = players.indexOf(player) + 1;
-            let emoji = `${index}`;
-            if (index == 1) emoji = ':first_place:' 
-            if (index == 2) emoji = ':second_place:' 
-            if (index == 3) emoji = ':third_place:' 
+            const index = players.indexOf(player);
+            let emoji = emojis[index];
 
             embed.addFields([
                 { name: `${emoji} - ${player.username}`, value: `${player.score} Pontos` }
-            ])
-        })
+            ]);
+        });
 
         await interaction.reply({ embeds: [embed], content: "Veja a seguir o ranking", ephemeral: true });
     }
