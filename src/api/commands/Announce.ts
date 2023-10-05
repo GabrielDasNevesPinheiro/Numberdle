e81cf1dda43d1ab6a24e763efbac421f49277220
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-05 13:49:10 -0300

    The Announce command

---
+++ src/api/commands/Announce.ts
@@ -0,0 +1,37 @@
+import { CacheType, CommandInteraction, SlashCommandBuilder, TextChannel } from "discord.js";
+import Command from "./Command";
+import Guild from "../../database/Models/Guild";
+
+
+export default abstract class Announce extends Command {
+
+    static command: SlashCommandBuilder = new SlashCommandBuilder()
+        .addStringOption(option =>
+            option.setName("text")
+                .setDescription("Sua mensagem, mestre")
+                .setRequired(true))
+        .setName("announce")
+        .setDescription("Anunciar algo para os usuários nos servers")
+
+    static async execute(interaction: CommandInteraction<CacheType>) {
+
+        await interaction.deferReply({});
+
+        await interaction.editReply({ content: "Enviando aviso aos servidores..." })
+
+        const announce = interaction.options.get("text").value as string;
+
+        interaction.client.guilds.cache.map(async (guild) => {
+
+            const { defaultChannel } = (await Guild.findOne({ where: { guildId: guild.id } }));
+
+            const channel = guild.channels.cache.get(defaultChannel) as TextChannel;
+
+            await channel.send(`:warning: AVISO IMPORTANTE :warning: \n${announce}`);
+
+        });
+
+        await interaction.editReply({ content: `Aviso enviado para todos os servidores.` })
+    }
+
+}
