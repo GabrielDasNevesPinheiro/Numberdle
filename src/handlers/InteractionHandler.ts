8368081b77245fa6d3263fbb07cd9aa3dc642ca3
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-05 12:33:29 -0300

    Handling setName command

--- src/handlers/InteractionHandler.ts
+++ src/handlers/InteractionHandler.ts
@@ -6,6 +6,7 @@ import SetChannel from "../api/commands/SetChannel";
 import Command from "../api/commands/Command";
 import Play from "../api/commands/Play";
 import PlayerScore from "../api/commands/PlayerScore";
+import SetName from "../api/commands/SetName";
 
 
 let commands: { [key: string]: typeof Command } = {
@@ -14,7 +15,8 @@ let commands: { [key: string]: typeof Command } = {
     "rank": Rank,
     "setchannel": SetChannel,
     "play": Play,
-    "player": PlayerScore
+    "player": PlayerScore,
+    "setname": SetName
 }
 
 export default function executeAction(cmdName: string, interaction: Interaction<CacheType>) {
