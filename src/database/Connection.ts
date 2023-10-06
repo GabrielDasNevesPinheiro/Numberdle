ca6a64a95b5fb70f46296364ce89cb8c946a5cef
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-05 21:12:28 -0300

    Database logging disable

--- src/database/Connection.ts
+++ src/database/Connection.ts
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize-typescript";
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './src/database/db/database.sqlite'
+    storage: './src/database/db/database.sqlite',
+    logging: false,
 });
 
 export default sequelize;
