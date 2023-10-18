77e13de4f21951a6e8b6b9d8b6aa164505f79bc1
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-18 16:46:52 -0300

    buildTipMessage finish

--- src/core/engine/GameEngine.ts
+++ src/core/engine/GameEngine.ts
@@ -1,6 +1,6 @@
 
 export default class GameEngine {
-    
+
 
     public score_multiplier = 100;
     public max_attempts = 10;
@@ -13,35 +13,39 @@ export default class GameEngine {
         return number;
     }
 
-    public buildTipMessage(random_number: number, adittional?: string, overwrite?: string) {
+    public buildTipMessage(args: { attempts_left: number, random_number: number, adittional?: string, overwrite?: string }) {
+
+        const { attempts_left, adittional, overwrite, random_number } = args;
 
         if (overwrite) {
-            return overwrite;
+            return args.overwrite;
         }
 
         let tip = "";
 
-        if(this.tip_range == 0) {
+        if (this.tip_range == 0) {
 
             let len = random_number.toString().length - 1;
             let toSub = Number(random_number.toString()[len]);
-    
+
             const minRange = random_number - toSub;
             const maxRange = (random_number + 10) - toSub;
-    
-            tip = `Você tem só mais 3 tentativas! Seu número está entre ${minRange} e ${maxRange}`;
-        
+
+            tip = `Você tem só mais ${attempts_left} tentativas! Seu número está entre ${minRange} e ${maxRange}`;
+
         }
 
         if (this.tip_range > 0) {
             const minRange = random_number - this.tip_range;
             const maxRange = random_number + this.tip_range;
-            tip = `Você tem só mais 3 tentativas! Seu número está entre ${minRange} e ${maxRange}`;
-        } 
+            tip = `Você tem só mais ${attempts_left} tentativas! Seu número está entre ${minRange} e ${maxRange}`;
+        }
 
         if (adittional) {
-            tip += `\n${adittional}`
+            tip += `Você tem só mais ${attempts_left} tentativas! ${adittional}`
         }
+
+        return tip;
 
     }
 }
