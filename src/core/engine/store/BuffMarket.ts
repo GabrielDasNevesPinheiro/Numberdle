0bfdb0756988b9aa750bb1825424eb92d8889e68
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-19 20:11:05 -0300

    "O Dialeto das Prima" Buff add

--- src/core/engine/store/BuffMarket.ts
+++ src/core/engine/store/BuffMarket.ts
@@ -1,6 +1,7 @@
 // All buffs are implemented staticly in the array.
 // when a player rolls his store, code will sort the buffs by the array.
 
+import { isPrime } from "../../utils/Utils";
 import { Playing } from "../Playing";
 import Buff from "../buffs/Buff";
 import { Attributes } from "../enum/Attributes";
@@ -10,7 +11,7 @@ export const BuffMarket = [
 
     new Buff({
         name: "Benção do Trapaceiro",
-        price: 200,
+        price: 150,
         rarity: Rarity.NORMAL,
         description: "Ao iniciar o jogo, te direi se o número é maior ou menor que 500",
         targets: [Attributes.TIP_TIME, Attributes.TIP_TYPE],
@@ -22,6 +23,24 @@ export const BuffMarket = [
             Playing.inGame[userId].playerEngine.tip_attempt = 10;
             Playing.inGame[userId].playerEngine.wrap_default_tip = true;
             Playing.inGame[userId].playerEngine.tip_message = `O número é ${generatedNumber >= 500 ? ">= 500" : "<= 500"}`;
+
+        }
+    }),
+    new Buff({
+        name: "O Dialeto das Prima",
+        price: 200,
+        rarity: Rarity.NORMAL,
+        description: '"As Prima sabe muito piá". No início do jogo você saberá se o número é primo ou não.',
+        targets: [Attributes.TIP_TIME, Attributes.TIP_TYPE],
+
+        apply: (userId: string) => {
+
+            const { generatedNumber } = Playing.inGame[userId];
+            const primo = isPrime(generatedNumber);
+
+            Playing.inGame[userId].playerEngine.tip_attempt = 10;
+            Playing.inGame[userId].playerEngine.wrap_default_tip = false;
+            Playing.inGame[userId].playerEngine.tip_message = `O número ${ primo ? "é primo" : "não é primo"}.`;
 
         }
     }),
