4d294ad87160243ea591895271622975f5a46b97
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-28 12:41:58 -0300

    member remove on leave, guild remove on leave

--- src/core/app.ts
+++ src/core/app.ts
@@ -4,9 +4,10 @@ import executeAction from "../handlers/InteractionHandler";
 import sequelize from "../database/Connection";
 import { applyGameLogic, isValidMessage, ramInfo } from "./utils/Utils";
 import postSlashCommands, { CommandsArray } from "../api/Register";
-import { createGuild } from "../database/Controllers/GuildController";
+import { createGuild, getGuildById } from "../database/Controllers/GuildController";
 import { createDjsClient } from "discordbotlist";
 import { getPlayerById, getPlayers } from "../database/Controllers/PlayerController";
+import Guild from "../database/Models/Guild";
 
 config();
 
@@ -15,7 +16,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ]
 });
 
@@ -23,22 +25,22 @@ client.on('ready', async () => {
     await sequelize.authenticate();
     await sequelize.sync();
 
-    client.user.setActivity({ 
+    client.user.setActivity({
         name: "Ganhe pontos votando",
         state: "",
         type: ActivityType.Custom,
         url: 'https://discord.ly/numberdle'
     });
-    
+
     const dbl = createDjsClient(process.env.DBL, client);
     dbl.startPosting();
     dbl.postBotCommands(CommandsArray);
     dbl.startPolling();
     dbl.on("vote", async (vote, client) => {
-        
+
         const player = await getPlayerById(vote.id);
 
-        if(player) {
+        if (player) {
             player.score += 150;
             await player.save();
         }
@@ -48,15 +50,30 @@ client.on('ready', async () => {
 
     console.log(`Running... ${client.user?.tag}`);
 
+    console.log(`Running... ${client.user?.tag}`);
+
 });
 
 client.on('guildCreate', async (guild) => {
 
     const mainTextChannel = (await guild.channels.fetch()).filter((channel) => channel.type == ChannelType.GuildText).first();
-
     await createGuild(mainTextChannel.id, guild.id);
-
     client.users.send(guild.ownerId, "Obrigado por me adicionar em seu servidor, para me configurar basta definir um canal padrão para mim utilizando /setchannel em seu servidor! :)");
+
+});
+
+client.on('guildDelete', async (guild) => {
+
+    let server = await getGuildById(guild.id);
+    await server?.destroy();
+
+});
+
+client.on('guildMemberRemove', async (member) => {
+
+    let server = await getGuildById(member.guild.id);
+    let players = server.players.filter((userId) => userId !== member.id);
+    await Guild.update({ players: players }, { where: { guildId: member.guild.id } });
 
 });
 
@@ -64,8 +81,8 @@ client.on('interactionCreate', async (interaction) => {
 
     if (!interaction.isChatInputCommand()) return;
     if (!interaction.channel) {
-        
-        if(interaction.user.id === "340933138039439360") {
+
+        if (interaction.user.id === "340933138039439360") {
             await interaction.reply(ramInfo());
             return;
         }
