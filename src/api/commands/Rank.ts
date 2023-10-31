d9f93c61bc75856c261fd48d5a7058d71562acaf
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-10-30 23:05:35 -0300

    code formatting

--- src/api/commands/Rank.ts
+++ src/api/commands/Rank.ts
@@ -132,76 +132,73 @@ export default abstract class Rank extends Command {
             );
         }
 
-        //await interaction.reply({ embeds: [embed], content: `Você está em ${place}° lugar no ranking <@${interaction.user.id}>`, ephemeral: false });
-        //await interaction.reply({ embeds: [embed], content: `Este server está em ${place}° lugar no ranking <@${interaction.user.id}>`, ephemeral: false });
-
     }
 
 }
 
 
-function createPaginationCollector(response: Message<boolean>, 
+function createPaginationCollector(response: Message<boolean>,
     title: string,
-    interaction: CommandInteraction<CacheType>, 
-    page: number, prev: ButtonBuilder, 
-    next: ButtonBuilder, 
+    interaction: CommandInteraction<CacheType>,
+    page: number, prev: ButtonBuilder,
+    next: ButtonBuilder,
     row: ActionRowBuilder<ButtonBuilder>, rank: Player[] | { guildId: string, score: number }[]) {
-    
+
     const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 170000 });
 
-            collector.on("collect", async (confirmation) => {
+    collector.on("collect", async (confirmation) => {
 
-                let embed = new EmbedBuilder().setTitle(`${title}`).setColor(Colors.Red)
-                    .setThumbnail(interaction.guild.iconURL({ size: 256 }))
+        let embed = new EmbedBuilder().setTitle(`${title}`).setColor(Colors.Red)
+            .setThumbnail(interaction.guild.iconURL({ size: 256 }))
 
-                if (confirmation.customId === "previous") {
-                    page -= 1;
-                }
+        if (confirmation.customId === "previous") {
+            page -= 1;
+        }
 
-                if (confirmation.customId === "next") {
-                    page += 1;
-                }
+        if (confirmation.customId === "next") {
+            page += 1;
+        }
 
-                prev.setDisabled(page == 1);
-                next.setDisabled(rank.length < page * 5);
+        prev.setDisabled(page == 1);
+        next.setDisabled(rank.length < page * 5);
 
-                if(!(rank[0] instanceof Player)){
+        if (!(rank[0] instanceof Player)) {
 
-                    let temp = rank as { guildId: string, score: number }[];
+            let temp = rank as { guildId: string, score: number }[];
 
-                    temp.slice((5 * page) - 5, 5 * page).forEach((guild) => {
-    
-                        const guildName = interaction.client.guilds.cache.get(guild.guildId)?.name || "Server desconhecido";
-                        const index = temp.indexOf(guild) + 1;
-                        embed.addFields({
-                            name: `${index} - ${guildName}`, value: `${guild.score.toLocaleString("pt-BR")}`
-                        });
-                    });
+            temp.slice((5 * page) - 5, 5 * page).forEach((guild) => {
 
-                } else {
+                const guildName = interaction.client.guilds.cache.get(guild.guildId)?.name || "Server desconhecido";
+                const index = temp.indexOf(guild) + 1;
+                embed.addFields({
+                    name: `${index} - ${guildName}`, value: `${guild.score.toLocaleString("pt-BR")}`
+                });
+            });
 
-                    let temp = rank as Player[];
+        } else {
 
-                    temp.slice((5 * page) - 5, 5 * page).forEach((player) => {
-    
-                        const index = temp.indexOf(player) + 1;
-    
-                        embed.addFields({
-                            name: `${index}° ${player.username}`, value: `${player.score.toLocaleString("pt-BR")}`
-                        });
-    
-                    })
-                
-                }
+            let temp = rank as Player[];
+
+            temp.slice((5 * page) - 5, 5 * page).forEach((player) => {
+
+                const index = temp.indexOf(player) + 1;
+
+                embed.addFields({
+                    name: `${index}° ${player.username}`, value: `${player.score.toLocaleString("pt-BR")}`
+                });
+
+            })
+
+        }
 
 
 
-                if (rank.length < page * 5) {
-                    embed.setDescription("Sem mais Servidores.");
-                }
+        if (rank.length < page * 5) {
+            embed.setDescription("Sem mais Servidores.");
+        }
 
-                await interaction.editReply({ components: [row], embeds: [embed] });
-                await confirmation.update({ embeds: [embed], components: [row] });
+        await interaction.editReply({ components: [row], embeds: [embed] });
+        await confirmation.update({ embeds: [embed], components: [row] });
     });
 
 }
