7feab2e9de8ab3e33b3c6de3f1d55fbffd477824
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-11 22:31:12 -0300

    Game fields add

--- src/database/Models/Game.ts
+++ src/database/Models/Game.ts
@@ -6,8 +6,12 @@ class Game extends Model<InferAttributes<Game>, InferCreationAttributes<Game>> {
     declare id: number;
     declare date: Date;
     declare userId: string;
+    declare attempts: number; // used attempts
+    declare multiplier: number; // multiplier after game result
+    declare score: number; // score after game result
+    declare earned: number; // earned points in this game
     declare win: boolean;
-    declare buffs: Array<Number>;
+    declare buffs: Array<Number>; // used buffs in this game
     declare guildId: string;
 
 }
@@ -24,19 +28,35 @@ Game.init({
     },
     userId: {
         type: new DataTypes.STRING(24),
-        allowNull: false
+        allowNull: false,
+    },
+    attempts: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    score: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    earned: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    multiplier: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
     },
     win: {
         type: DataTypes.BOOLEAN,
-        allowNull: false
+        allowNull: false,
     },
     buffs: {
         type: DataTypes.JSON(),
-        defaultValue: []
+        defaultValue: [],
     },
     guildId: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     }
 }, {
     sequelize,
