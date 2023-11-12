17604f5196198564387971e8146748e7b8e078d4
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-12 01:33:46 -0300

    buffs count per roll game setting

--- src/core/engine/GameSettings.ts
+++ src/core/engine/GameSettings.ts
@@ -1,5 +1,7 @@
 
 export default class GameSettings {
-    static cooldown = 24;
-    static roleplayMaxTime = 240 * 1000
+    static cooldown = 24; // hours
+    static roleplayMaxTime = 240 * 1000 // ms
+    static rollCooldown = 72 // hours
+    static buffsPerRoll = 7
 }
